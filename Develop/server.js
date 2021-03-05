679edefd02bc3baa110f572befad236d4cd6670c
Dan Bushong <bushongdan@gmail.com>
2021-03-04 19:07:53 -0600

    setting up the server for heroku and atlas deployment

--- Develop/server.js
+++ Develop/server.js
@@ -3,8 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-//
-const PORT = 3000;
+//changed to include env and mongo port
+const PORT = process.env.PORT || 27017;
 
 const app = express();
 
@@ -16,11 +16,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-//
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+//need to change config vars in heroku to reflect atlas information
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {useNewUrlParser: true, useUnifiedTopology: true});
 
 // routes
 app.use(require("./routes/api.js"));
